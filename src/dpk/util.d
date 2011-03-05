f2f882cd90aa0f03d5357a5256ef2e0b108fe3d1
dawg <dawg@dawgfoto.de>
2011-03-05 16:44:02 +0100

    imported file operations

--- src/dpk/util.d
+++ src/dpk/util.d
@@ -49,6 +49,64 @@ struct DirChanger {
   ~this() { std.file.chdir(this.olddir); }
 }
 
+string[] copyRel(string tgtdir, string globs, string root = std.path.curdir) {
+  tgtdir = rel2abs(tgtdir);
+  if (!exists(tgtdir))
+    mkdirRecurse(tgtdir);
+  enforce(isDir(tgtdir), new Exception(
+            fmtString("Installation failed, %s exists and is no dir.", tgtdir)));
+
+  auto dc = DirChanger(root);
+  auto dirs = resolveGlobs!(unaryFun!(q{a.isDir}))("**");
+
+  string[] createdDirs;
+  scope(failure) foreach(dir; createdDirs) { std.file.rmdir(dir); }
+
+  foreach(dir; dirs) {
+    auto subdir = join(tgtdir, dir);
+    if (!exists(subdir)) {
+      createdDirs ~= subdir;
+      mkdirRecurse(subdir);
+    } else {
+      enforce(isDir(subdir), new Exception(
+                fmtString("Installation failed, %s exists and is no dir.", subdir)));
+    }
+  }
+
+  auto files = resolveGlobs!(unaryFun!(q{a.isFile}))(globs);
+  foreach(file; files) {
+    auto tgtfile = join(tgtdir, file);
+    if (std.file.exists(tgtfile))
+      std.file.remove(tgtfile);
+
+    std.file.copy(file, tgtfile);
+  }
+  string prefixDir(string path) {
+    return std.path.join(root, path);
+  }
+  return apply!prefixDir(files);
+}
+
+bool isDirEmpty(string dir) {
+  foreach(string _; dirEntries(dir, SpanMode.shallow)) {
+    return false;
+  }
+  return true;
+}
+
+void removeFile(string file) {
+  try {
+    std.file.remove(file);
+    auto dir = dirname(file);
+    while (isDirEmpty(dir)) {
+      std.file.rmdir(dir);
+      dir = dirname(dir);
+    }
+  } catch (Exception e) {
+    std.stdio.stderr.writeln(e.toString());
+  }
+}
+
 private:
 
 auto reMatcher(string dpkmatcher) {
