b465d5eeca7ea7fd4624d6e96532d1dfda6c8e9c
dawg <dawg@dawgfoto.de>
2011-06-26 23:43:00 +0200

    run unittests after build and leave dpk_boot.exe in place

--- boot.bat
+++ boot.bat
@@ -1,3 +1,5 @@
 cmd /C dmd -debug -unittest -ofdpk_boot src\dpk\build.d src\dpk\config.d src\dpk\ctx.d src\dpk\dflags.d src\dpk\install.d src\dpk\main.d src\dpk\pkgdesc.d src\dpk\util.d src\dpk\utrunner.d
-dpk_boot.exe
-del dpk_boot.exe dpk_boot.obj
+@echo RUNNING UNITTESTS
+@echo off
+del dpk_boot.obj
+dpk_boot test
